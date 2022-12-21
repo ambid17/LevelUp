6f612a725ec1caaaddc4341a3a918279d30ca50d
AudioDread <84207113+AudioDread@users.noreply.github.com>
2022-12-20 19:09:28 -0800

    Commented my script

--- Assets/Minigames/Engineering/Scripts/Controllers/TerrainSpawner.cs
+++ Assets/Minigames/Engineering/Scripts/Controllers/TerrainSpawner.cs
@@ -12,10 +12,14 @@ public class TerrainSpawner : MonoBehaviour
 
     private void Awake()
     {
+        //Randomizes the first terrain chunk from the blank terrain list so you don't get spawn camped
         int firstSpawn = Random.Range(0, TerrainManager.BlankTerrain.Count);
+        //sets the first chunk of terrain to spawn directly beneath the player
         Vector2 initSpawnPos = new Vector2(PlayerPos().x, PlayerPos().y - initSpawnHeight);
         Tilemap initTerrain = TerrainManager.BlankTerrain[firstSpawn];
+        //instantiates initial terrain chunk and assigns it to the singleton
         TerrainManager.CurrentTerrain = Instantiate(initTerrain, initSpawnPos, initTerrain.transform.rotation, TerrainManager.Grid);
+        //generates the rest of the terrain up to the max distance before first frame
         while (Vector2.Distance(PlayerPos(), TerrainManager.CurrentTerrain.transform.position) < spawnDist)
         {
             RandomizeTerrain();
@@ -23,6 +27,7 @@ public class TerrainSpawner : MonoBehaviour
     }
     private void Update()
     {
+        //spawns new terrain as player vehicle approaches
         if (Vector2.Distance(PlayerPos(), TerrainManager.CurrentTerrain.transform.position) < spawnDist)
         {
             RandomizeTerrain();
@@ -30,6 +35,7 @@ public class TerrainSpawner : MonoBehaviour
     }
     private void RandomizeTerrain()
     {
+        //DnD style encounter check, roll a D20 to determine if you get a hazard, enemy, both, or nothing
         int RNG = Random.Range(0, 21);
         List<Tilemap> tempList = new List<Tilemap>();
         switch (RNG)
@@ -47,15 +53,21 @@ public class TerrainSpawner : MonoBehaviour
                 tempList = TerrainManager.BlankTerrain;
                 break;
         }
+        //passes in the correct list to spawn from
         SpawnTerrain(tempList);
     }
     private void SpawnTerrain(List<Tilemap> list)
     {
+        //randomizes an entry from the provided list
         int RNG = Random.Range(0, list.Count);
+        //calculates the current rightmost edge of the map in world space
         float currentEdge = TerrainManager.CurrentTerrain.transform.TransformPoint(TerrainManager.CurrentTerrain.cellBounds.max).x;
+        //calculates the distance between the leftmost edge of the next Tilemap to be instantiated, and that Tilemap's transform
         float newDiff = Mathf.Abs(list[RNG].cellBounds.xMin - list[RNG].transform.position.x);
+        //determines the correct position on the X axis for the next Tilemap based on the calculations
         float newX = currentEdge + newDiff;
         Vector2 newPos = new Vector2(newX, TerrainManager.CurrentTerrain.transform.position.y);
+        //instantiates the new map in the correct position and sets it as the new current terrain in the singleton
         TerrainManager.CurrentTerrain = Instantiate(list[RNG], newPos, list[RNG].transform.rotation, TerrainManager.Grid);
     }
     private Vector2 PlayerPos()
