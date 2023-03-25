51bd79ca801146fa157c6f35429c800fe38f4ca0
Sabien <ambid17@gmail.com>
2023-03-25 12:40:02 -0500

    Allow "delete save data" to clear scriptable objects

--- Assets/Scripts/Utils/Editor/DeleteAllPlayerPrefs.cs
+++ Assets/Scripts/Utils/Editor/DeleteAllPlayerPrefs.cs
@@ -3,16 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using System.IO;
+using Minigames.Fight;
 
 public class DeleteAllPlayerPrefs : MonoBehaviour
 {
     [MenuItem("Caos Creations/Delete save data")]
-    static void DeletePlayerPrefs()
+    static void DeleteSaveData()
     {
         var paths = Directory.EnumerateFiles(Application.persistentDataPath);
         foreach(var path in paths) 
         {
             File.Delete(path);
         }
+
+        ResetScriptableObjects();
+    }
+
+    static void ResetScriptableObjects()
+    {
+        var progress = AssetDatabase.LoadAssetAtPath<ProgressSettings>("Assets/Minigames/Fight/ScriptableObjects/ProgressSettings.asset");
+        progress.SetDefaults();
+        EditorUtility.SetDirty(progress);
+        
+        var weaponSettings = AssetDatabase.LoadAssetAtPath<WeaponSettings>("Assets/Minigames/Fight/ScriptableObjects/WeaponSettings.asset");
+        weaponSettings.SetDefaults();
+        EditorUtility.SetDirty(weaponSettings);
+        
+        var effectSettings = AssetDatabase.LoadAssetAtPath<EffectSettings>("Assets/Minigames/Fight/ScriptableObjects/EffectSettings.asset");
+        effectSettings.SetDefaults();
+        EditorUtility.SetDirty(effectSettings);
+        
+        AssetDatabase.SaveAssets();
     }
 }
