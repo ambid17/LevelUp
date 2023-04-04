0e8ff84aa3d8d4a3f65713195ea46d722e7aba30
AudioDread <84207113+AudioDread@users.noreply.github.com>
2023-04-03 23:25:43 -0700

    Set up Bee behavior

--- Assets/Minigames/Fight/Scripts/Behavior/EntityBehaviorData.cs
+++ Assets/Minigames/Fight/Scripts/Behavior/EntityBehaviorData.cs
@@ -12,8 +12,10 @@ namespace Minigames.Fight
         public float MoveSpeed => entity.enemyStats.MoveSpeed;
         public GameObject PlayerGo => entity.Target.gameObject;
         public Transform Player => entity.Target;
+        public Vector2 PlayerVector => entity.Target.position;
         public List<Transform> Waypoints => GameManager.EnemySpawnManager.Waypoints;
         public float CurrentHealth => entity.Stats.currentHp;
         public float DamageTaken => entity.enemyStats.MaxHp - entity.Stats.currentHp;
+        public float DistanceToPlayer => Vector2.Distance(transform.position, PlayerVector);
     }
 }
