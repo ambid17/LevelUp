e163e89ec410b64d1123b4f94d2ffefd10a1776f
Sabien <ambid17@gmail.com>
2023-04-16 19:58:51 -0500

    fix bullet de-spawn

--- Assets/Scripts/Utils/PhysicsUtils.cs
+++ Assets/Scripts/Utils/PhysicsUtils.cs
@@ -4,7 +4,7 @@ public static class PhysicsUtils
 {
     public static readonly int PlayerLayer = 6;
     public static int ProjectileLayer = 7;
-    public static int GroundLayer = 8;
+    public static int GroundLayer = 10;
     public static int EnemyLayer = 9;
 
     public static Vector2 AsVector2(this Vector3 _v)
