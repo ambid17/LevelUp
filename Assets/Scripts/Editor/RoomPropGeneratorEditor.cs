aa4ffed672b68f11223dec3e75969163a53cdf07
AudioDread <84207113+AudioDread@users.noreply.github.com>
2023-08-12 22:14:57 -0700

    Set up editor script for prop generation

--- Assets/Scripts/Editor/RoomPropGeneratorEditor.cs
+++ Assets/Scripts/Editor/RoomPropGeneratorEditor.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
-public class RoomPropGeneratorEditor : MonoBehaviour
+[CustomEditor(typeof(RoomPropGenerator))]
+public class RoomPropGeneratorEditor : UnityEditor.Editor
 {
-    // Start is called before the first frame update
-    void Start()
+    public override void OnInspectorGUI()
     {
-        
-    }
+        DrawDefaultInspector();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        RoomPropGenerator myScript = (RoomPropGenerator)target;
+        if (GUILayout.Button("Generate Props"))
+        {
+            myScript.GenerateProps();
+        }
     }
 }
