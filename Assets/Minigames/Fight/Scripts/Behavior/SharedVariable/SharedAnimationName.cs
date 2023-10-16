81e8afbfe36dd02cba44ee9eab51dbb03f216a59
ambid17 <ambid17@gmail.com>
2023-10-16 18:33:39 -0500

    move all scripts

--- Assets/Minigames/Fight/Scripts/Behavior/SharedVariable/SharedAnimationName.cs
+++
@@ -1,8 +0,0 @@
-namespace BehaviorDesigner.Runtime
-{
-    [System.Serializable]
-    public class SharedAnimationName : SharedVariable<AnimationName>
-    {
-        public static implicit operator SharedAnimationName(AnimationName value) { return new SharedAnimationName { Value = value }; }
-    }
-}
