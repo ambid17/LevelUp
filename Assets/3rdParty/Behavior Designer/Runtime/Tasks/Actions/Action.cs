b760f38886c5619e2517f00a126ae0e0ba9a9015
AudioDread <84207113+AudioDread@users.noreply.github.com>
2023-12-01 18:28:07 -0800

    Revert "Fixed Wasp"
    This reverts commit 5755da5d769cfeacf6fde498025e9fe12fe95f9a.

--- Assets/3rdParty/Behavior Designer/Runtime/Tasks/Actions/Action.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BehaviorDesigner.Runtime.Tasks
-{
-    // Action classes alter the state of the game. For example, an action task might consist of playing an animation or firing a weapon.
-    public abstract class Action : Task
-    {
-        // Intentionally left blank
-    }
-}
