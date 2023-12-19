0169c82940e1120c9063a6a64e3a14c9b74aa69d
ambid17 <ambid17@gmail.com>
2023-12-18 20:34:16 -0600

    clean up prop sway code

--- Assets/Scripts/Props/PropSway.cs
+++ Assets/Scripts/Props/PropSway.cs
@@ -6,116 +6,55 @@ using UnityEngine;
 
 public class PropSway : MonoBehaviour
 {
-    public SpriteRenderer ShadowRenderer;
-
-    [SerializeField]
-    private SpriteRenderer spriteRenderer;
-    [SerializeField]
-    private float strength = 2f;
-    [SerializeField]
-    private float speed = 5f;
-    [SerializeField]
-    private float decelRate = 2f;
-    [SerializeField]
-    private float triggerDistance = .5f;
-    [SerializeField]
-    private float timeBetweenTriggers = .5f;
-
-    private int _center = Shader.PropertyToID("_Center");
-    private int _speed = Shader.PropertyToID("_Speed");
-    private int _strength = Shader.PropertyToID("_Strength");
-
-    private float _minSpeed;
-    private float _minStrength;
+    [Header("Set In Editor")]
+    [SerializeField] private SpriteRenderer shadowRenderer;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private float strength = 2f;
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float decelRate = 2f;
+    [SerializeField] private float timeBetweenTriggers = .5f;
+    
+    [Header("Used for debugging, set at runtime")]
+    [SerializeField] private float _minSpeed;
+    [SerializeField] private float _minStrength;
+    [SerializeField] private float _triggerTimer;
 
     private Material _myMat;
     private Material _shadowMat;
 
-    private List<Transform> _enteredEntities = new();
-    private List<Transform> _triggeredEntities = new();
-    private float _triggerTimer;
-
-    [ContextMenu("GetComponent")]
-    public void GetComponent()
-    {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-    }
+    private int _speed = Shader.PropertyToID("_Speed");
+    private int _strength = Shader.PropertyToID("_Strength");
 
     private void Awake()
     {
         _myMat = spriteRenderer.material;
-        if (ShadowRenderer != null)
+        if (shadowRenderer != null)
         {
-            _shadowMat = ShadowRenderer.material;
+            _shadowMat = shadowRenderer.material;
         }
-        float centerX = spriteRenderer.sprite.pivot.x / spriteRenderer.sprite.rect.width;
-        float centerY = spriteRenderer.sprite.pivot.y / spriteRenderer.sprite.rect.height;
 
         _minSpeed = _myMat.GetFloat(_speed);
         _minStrength = _myMat.GetFloat(_strength);
-
-        _myMat.SetVector(_center, new Vector2(centerX, centerY));
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.layer == PhysicsUtils.PlayerLayer || collision.gameObject.layer == PhysicsUtils.EnemyLayer || collision.gameObject.layer == PhysicsUtils.ProjectileLayer)
         {
-            if (!_enteredEntities.Contains(collision.transform))
-            {
-                _enteredEntities.Add(collision.transform);
-            }
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.gameObject.layer == PhysicsUtils.PlayerLayer || collision.gameObject.layer == PhysicsUtils.EnemyLayer || collision.gameObject.layer == PhysicsUtils.ProjectileLayer)
-        {
-            _enteredEntities.Remove(collision.transform);
-        }
-    }
-
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        foreach (Transform t in _enteredEntities.ToList())
-        {
-            // Projectiles move so fast that they sometimes leave the trigger area before the next physics update
-            // and will sometimes destroy themselves while inside of the trigger area
-            // this failsafe will ensure we never check a null transform.
-            if (t == null)
-            {
-                _enteredEntities.Remove(t);
-                continue;
-            }
-            if (Vector2.Distance(t.position, transform.position) <= triggerDistance)
-            {
-                _enteredEntities.Remove(t);
-                _triggeredEntities.Add(t);
-                Shake();
-            }
-        }
-        foreach (Transform t in _triggeredEntities.ToList())
-        {
-            if (t == null)
-            {
-                _triggeredEntities.Remove(t);
-                continue;
-            }
-            if (Vector2.Distance(t.position, transform.position) > triggerDistance)
-            {
-                _triggeredEntities.Remove(t);
-                _enteredEntities.Add(t);
-                Shake();
-            }
+            Shake();
         }
     }
 
     private void Update()
     {
         _triggerTimer += Time.deltaTime;
-        float newSpeed = Mathf.Clamp(_myMat.GetFloat(_speed) - Time.deltaTime * decelRate * (speed/strength), _minSpeed, speed);
-        float newStrength = Mathf.Clamp(_myMat.GetFloat(_strength) - Time.deltaTime * decelRate, _minStrength, strength);
+
+        float newSpeed = _myMat.GetFloat(_speed) - Time.deltaTime * decelRate * (speed / strength);
+        newSpeed = Mathf.Clamp(newSpeed, _minSpeed, speed);
+
+        float newStrength = _myMat.GetFloat(_strength) - Time.deltaTime * decelRate;
+        newStrength = Mathf.Clamp(_myMat.GetFloat(_strength) - Time.deltaTime * decelRate, _minStrength, strength);
+
         _myMat.SetFloat(_speed, newSpeed);
         _myMat.SetFloat(_strength, newStrength);
         if (_shadowMat != null)
@@ -139,5 +78,11 @@ public class PropSway : MonoBehaviour
             _shadowMat.SetFloat(_speed, speed);
             _shadowMat.SetFloat(_strength, strength);
         }
+    }
+
+    [ContextMenu("GetComponent")]
+    public void GetComponent()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 }
