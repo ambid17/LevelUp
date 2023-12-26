bf1b0414f127720c206d434f6b07c36d04694b5d
AudioDread <84207113+AudioDread@users.noreply.github.com>
2023-12-26 14:38:15 -0800

    Improved layering of construction chamber

--- Assets/Scripts/Utils/Sprites/StaticSpriteSort.cs
+++ Assets/Scripts/Utils/Sprites/StaticSpriteSort.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-    public class StaticSpriteSort : MonoBehaviour
-    {
+public class StaticSpriteSort : MonoBehaviour
+{
+    [SerializeField]
+    private float offset = 0;
         void Awake()
-        {
-            var spriteRenderer = GetComponent<SpriteRenderer>();
-            spriteRenderer.sortingOrder = transform.GetSortingOrder();
-        }
+    {
+        var spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sortingOrder = transform.GetSortingOrder(offset);
     }
+}
